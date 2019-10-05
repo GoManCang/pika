a985732a843723467c148e21378968a28c7010b0
lishulong <lishulong.never@gmail.com>
2019-10-05 21:15:29 +0800

    feat: file op

--- so/src/file.go
+++ so/src/file.go
@@ -1,50 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func FileTest() {
-	//fileName := "c://this.txt"
+func createFIle(fileName string) *os.File {
 	/*
-		open c://this.txt: no such file or directory
-		文件关闭异常： invalid argument
+		//fileName := "c://this.txt"
+			open c://this.txt: no such file or directory
+			文件关闭异常： invalid argument
 	*/
-	fileName := "this.txt"
+
 	fp, err := os.Create(fileName)
 	if err != nil {
 		//文件创建失败，路径不存在，文件全县，程序打开文件上限
 		fmt.Println(err)
 	}
 	for i := 1; i <= 10; i++ {
-		_, _ = fp.WriteString("hello world\n")
+		_, _ = fp.WriteString("hello world" + strconv.FormatInt(int64(i), 10) + "\n")
 
 	}
-	//err = fp.Close()
-	//if err != nil {
-	//	fmt.Println("文件关闭异常：", err)
-	//}
-
 	defer fp.Close()
+	return fp
+}
 
-	//追加
-	//count, _ := fp.Seek(0, io.SeekEnd)
-	//
-	//_, _ = fp.WriteAt([]byte("lishulong"), count)
-
-	//读取内容
-	fp, err = os.Open(fileName)
+func readFile(fileName string) {
+	fp, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println("打开文件失败：", err)
 	}
-	//buf := make([]byte, 1024)
-	//n, _ := fp.Read(buf)
-	//fmt.Println(string(buf[:n]))
-	//defer fp.Close()
+	buf := make([]byte, 1024)
+	n, _ := fp.Read(buf)
+	fmt.Println(string(buf[:n]))
+	defer fp.Close()
+}
 
-	//分段读取
+func readFileBySegment(fileName string) {
+	fp, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("打开文件失败：", err)
+	}
 	buf := make([]byte, 10)
 	for {
 		n, err := fp.Read(buf)
@@ -54,6 +54,60 @@ func FileTest() {
 		}
 		fmt.Print(string(buf[:n]))
 	}
+	defer fp.Close()
+}
+
+func appendFile(fp *os.File) {
+	//追加
+	count, _ := fp.Seek(0, io.SeekEnd)
+	_, _ = fp.WriteAt([]byte("lishulong"), count)
+
+}
+
+func cacheReadFile(fileName string) {
+	fp, err := os.OpenFile(fileName, os.O_RDONLY, 6)
+	if err != nil {
+		fmt.Println("文件打开失败：", err)
+		return
+	}
+	defer fp.Close()
+
+	r := bufio.NewReader(fp)
+	//slice, _ := r.ReadBytes('\n')
+	//fmt.Println(string(slice))
+	//
+	//slice, _ = r.ReadBytes('\n')
+	//fmt.Println(string(slice))
+	for {
+		//acsii 的任意值
+		slice, err := r.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Print(string(slice))
+	}
+}
+
+func FileTest() {
+
+	fileName := "this.txt"
+	fp := createFIle(fileName)
+	err := fp.Close()
+	if err != nil {
+		fmt.Println("文件关闭异常：", err)
+	}
+
+	//追加
+	appendFile(fp)
+	//读取内容
+	readFile(fileName)
+	//分段读取
+	readFileBySegment(fileName)
+	//
+
+	fmt.Println(strings.Repeat("-", 20))
+
+	cacheReadFile(fileName)
 
 	_ = os.Remove(fileName)
 
