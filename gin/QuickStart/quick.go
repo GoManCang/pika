d6fff50304ad0b92757485c6b4bebbf1ba3bbf07
lishulong <lishulong.never@gmail.com>
2019-10-08 19:31:30 +0800

    feat: gin quick start

--- gin/QuickStart/quick.go
+++ gin/QuickStart/quick.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 	r := gin.Default()
+	r.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err == nil {
+		fmt.Println("启动正常")
+	} else {
+		fmt.Println("启动异常：", err)
+	}
 }
