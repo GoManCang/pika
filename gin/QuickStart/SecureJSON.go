45e89b04f7308dfc0b2d7b8e8d550e0d0468f07d
lishulong <lishulong.never@gmail.com>
2019-10-12 19:31:42 +0800

    feat: secureJson 防止json劫持

--- gin/QuickStart/SecureJSON.go
+++ gin/QuickStart/SecureJSON.go
@@ -15,4 +15,16 @@ func main() {
 	})
 
 	_ = r.Run(":9000")
+
+	/*
+		➜  ~ http http://localhost:9000/someJson
+		HTTP/1.1 200 OK
+		Content-Length: 31
+		Content-Type: application/json; charset=utf-8
+		Date: Sat, 12 Oct 2019 11:30:20 GMT
+
+		while(1);["lena","asta","foot"]
+
+		➜  ~
+	*/
 }
