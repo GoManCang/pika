e8cd299292880b742ff34087de6f8d7f30456479
lishulong <lishulong.never@gmail.com>
2019-10-19 23:49:07 +0800

    feat：函数

---
+++ so/src/func.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func Test(a int, b int) (ret int) {
+	/*
+		参数可以命名 也可以不命名
+	*/
+	ret = a + b
+	return ret
+}
+
+func test(a int, b int) int {
+	return a + b
+}
+
+func test1(a, b, c int) int {
+	/*
+		类型声明简写
+	*/
+	return a + b + c
+}
+
+func test3(a string, y ...int) {
+	/*
+		允许传送多个参数 可变参数
+	*/
+	fmt.Println(a, y)
+
+}
+
+func main() {
+	ret := Test(1, 2)
+	fmt.Println(ret)
+
+	i := test(12, 33)
+	fmt.Println(i)
+
+	test3("1", 2, 3, 4, 5)
+
+	defer fmt.Println("hello deffer")
+	fmt.Println("after defer")
+
+}
